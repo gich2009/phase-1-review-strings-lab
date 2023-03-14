7fc977029c54489bc97ed7a70df83c9ee1a8f538
gich2009 <waneneamos@gmail.com>
2023-03-14 20:19:17 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code in this file!
+let currentUser = "John Mbuthi";
+
+const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+
+let upperCaseCurrentUser = currentUser.toUpperCase();
+
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + upperCaseCurrentUser + "!";
+
+let firstInitial = currentUser[0];
+
+const shortGreeting = "Welcome, " + firstInitial + "!";
