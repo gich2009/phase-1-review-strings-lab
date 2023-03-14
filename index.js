44dba51d09aae04f245043ae55354cb2ec1259bb
gich2009 <waneneamos@gmail.com>
2023-03-14 20:27:49 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 // Write your code in this file!
+
+//METHOD 1: 
+
 let currentUser = "John Mbuthi";
 
-const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
-let upperCaseCurrentUser = currentUser.toUpperCase();
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
 
-const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + upperCaseCurrentUser + "!";
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
 
-let firstInitial = currentUser[0];
 
-const shortGreeting = "Welcome, " + firstInitial + "!";
+//METHOD 2:
+
+// let currentUser = "John Mbuthi";
+
+// const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+
+// let upperCaseCurrentUser = currentUser.toUpperCase();
+
+// const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + upperCaseCurrentUser + "!";
+
+// let firstInitial = currentUser[0];
+
+// const shortGreeting = "Welcome, " + firstInitial + "!";
